9479a3f6279001fb314516e2624e763e05a636a8
Club Fitnes 2020 <clubfitnes2020@gmail.com>
2024-05-10 19:54:58 -0500

    first commit

---
+++ src/main/java/unfv/edu/pe/ApiSb2RestBdDtoApplication.java
@@ -0,0 +1,13 @@
+package unfv.edu.pe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSb2RestBdDtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSb2RestBdDtoApplication.class, args);
+	}
+
+}
