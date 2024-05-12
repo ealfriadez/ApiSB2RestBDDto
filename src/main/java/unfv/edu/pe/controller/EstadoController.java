3cf09418d979109b1746f0a8b82066cde6ee8f13
Club Fitnes 2020 <clubfitnes2020@gmail.com>
2024-05-12 01:06:33 -0500

    Continuando

--- src/main/java/unfv/edu/pe/controller/EstadoController.java
+++ src/main/java/unfv/edu/pe/controller/EstadoController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -61,5 +62,14 @@ public class EstadoController {
 				new ResourceNotFoundException("Recurso con el id: " + id + " no encontrado"));
 		estadoactualizar.setDescestado(estado.getDescestado());
 		return new ResponseEntity<>(service.guardar(estadoactualizar), HttpStatus.OK);	
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> eliminarEstado(
+			@PathVariable("id") long id){
+		service.obtenerPorId(id)
+		.orElseThrow(()->
+		new ResourceNotFoundException("Recurso con el id: " + id + " no encontrado"));
+		return ResponseEntity.status(HttpStatus.OK).body(service.eliminarPorId(id));
 	}
 }
