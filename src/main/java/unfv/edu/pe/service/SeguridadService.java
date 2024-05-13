606f288e55d2b2212f0c5e55cf6a01babe68be77
Eleazar Alfriadez <ealfriadez@gmail.com>
2024-05-13 16:03:52 -0500

    Continuando con la capa de seguridada

--- src/main/java/unfv/edu/pe/service/SeguridadService.java
+++ src/main/java/unfv/edu/pe/service/SeguridadService.java
@@ -1,10 +1,13 @@
 package unfv.edu.pe.service;
 
+import java.util.Iterator;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import unfv.edu.pe.model.security.RolUsuario;
 import unfv.edu.pe.model.security.Usuario;
 import unfv.edu.pe.repository.RolUsuarioRepository;
 import unfv.edu.pe.repository.UsuarioRepository;
@@ -24,5 +27,14 @@ public class SeguridadService {
 			return Optional.empty();
 		}
 		return entityOptional;
+	}
+	
+	public String[] listarRolesPorUsuario(Long idusuario) {
+		List<RolUsuario> rolesusuario = rolUsuarioRepository.listarRolesPorUsuario(idusuario);
+		String[] listarRoles = new String[rolesusuario.size()];
+		for (int i = 0; i < rolesusuario.size(); i++) {
+			listarRoles[i] = rolesusuario.get(i).getNomrol();			
+		}
+		return listarRoles;
 	}
 }
