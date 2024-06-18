9331236e95e44f0ef3de94f626df9eb6e9a44678
Eleazar Alfriadez <ealfriadez@gmail.com>
2024-06-18 14:59:35 -0500

    Retomando API REST QBO

--- src/main/java/unfv/edu/pe/ApiSb2RestBdDtoApplication.java
+++ src/main/java/unfv/edu/pe/ApiSb2RestBdDtoApplication.java
@@ -2,16 +2,11 @@ package unfv.edu.pe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class ApiSb2RestBdDtoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiSb2RestBdDtoApplication.class, args);
-	}
-
-	class WebSecurityConfig extends WebSecurityConfigurerAdapter{
-		
-	}
+	}	
 }
